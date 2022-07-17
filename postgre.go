b905d7bf3c96bffee67d23fc02ce9272e69c5359
Egor Kondachkov <pon4ik0123@mail.ru>
2022-07-17 10:00:40 +0300

    Update postgre.go

--- postgre.go
+++ postgre.go
@@ -21,13 +21,13 @@ func IsExist(Name string) (bool, error) {
 }
 
 func insert(Name, Size, Date, Price, Buy, Source string) (int64, error) {
-	check, err := IsExist(Name)
-	if err != nil {
-		return 0, err
-	}
-	if check == true {
-		return 0, nil
-	}
+	//check, err := IsExist(Name)
+	//if err != nil {
+	//	return 0, err
+	//}
+	//if check == true {
+	//	return 0, nil
+	//}
 	res, err := Database.Exec("INSERT INTO database_leaks VALUES ($1, $2,$3,$4,$5,$6)",
 		Name, Size, Date, Price, Buy, Source)
 	if err != nil {
