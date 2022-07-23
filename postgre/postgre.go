2d109348b5710260bc6bb1bf2965584d10750c17
Egor Kondachkov <pon4ik0123@mail.ru>
2022-07-23 12:45:26 +0300

    Update IsExist function

--- postgre/postgre.go
+++ postgre/postgre.go
@@ -1,6 +1,9 @@
 package postgre
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+)
 
 type Record struct {
 	Name   string
@@ -14,31 +17,27 @@ type Record struct {
 var Database *sql.DB
 
 func IsExist(Name string) (bool, error) {
-	response, err := Database.Query("SELECT EXISTS (SELECT * FROM database_leaks WHERE \"Name\" = $1)",
-		Name)
-	if err != nil {
-		return true, err
+	query := fmt.Sprintf("SELECT \"Name\" FROM database_leaks where \"Name\" = $1 limit 1")
+	row := Database.QueryRow(query, Name)
+	var tmp interface{}
+	err := row.Scan(&tmp)
+	if err == sql.ErrNoRows {
+		return false, nil
 	}
-
-	var result bool
-	err = response.Scan(result)
-
-	if err != nil {
-		return result, err
+	if err == nil {
+		return true, nil
 	}
-
-	return result, nil
-
+	return false, err
 }
 
 func Insert(Name, Size, Date, Price, Buy, Source string) (int64, error) {
-	//check, err := IsExist(Name)
-	//if err != nil {
-	//	return 0, err
-	//}
-	//if check == true {
-	//	return 0, nil
-	//}
+	check, err := IsExist(Name)
+	if err != nil {
+		return 0, err
+	}
+	if check == true {
+		return 0, nil
+	}
 	res, err := Database.Exec("INSERT INTO database_leaks VALUES ($1, $2,$3,$4,$5,$6)",
 		Name, Size, Date, Price, Buy, Source)
 	if err != nil {
